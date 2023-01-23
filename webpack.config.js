4414a23865326d17e58c240c7bee0d1ef1a5ae31
Kevin Batdorf <kbat82@gmail.com>
2023-01-22 23:12:57 -0500

    decrease bundle size

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,15 @@ module.exports = {
         ...defaultConfig.plugins,
         new CopyPlugin({
             patterns: [
-                { from: 'node_modules/shiki', to: 'shiki' },
+                { from: 'node_modules/shiki/dist', to: 'shiki/dist' },
+                {
+                    from: 'node_modules/shiki/themes/light-plus.json',
+                    to: 'shiki/themes',
+                },
+                {
+                    from: 'node_modules/shiki/languages/css.tmLanguage.json',
+                    to: 'shiki/languages',
+                },
                 { from: 'src/fonts', to: 'fonts' },
             ],
         }),
