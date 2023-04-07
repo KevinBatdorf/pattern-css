142951b67f6fe58f5dafc085e116801816a199f5
Kevin Batdorf <kbat82@gmail.com>
2023-04-07 17:16:47 -0400

    Fix lint issues

--- cypress/support/features/blocks.js
+++ cypress/support/features/blocks.js
@@ -1,8 +1,7 @@
 export const addCodeToCurrentBlock = (code) => {
     cy.openSideBarPanel('Additional CSS');
+    cy.get('[data-cy="ppc-editor-block"] textarea').should('exist').click();
     cy.get('[data-cy="ppc-editor-block"] textarea')
-        .should('exist')
-        .click()
         .should('have.focus')
         .type(code, { parseSpecialCharSequences: false });
 };
